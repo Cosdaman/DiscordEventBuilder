dc7a90ce0cf68eab2129fc184ca8f4232819ab3c
Cosdaman <Cosdaguy@gmail.com>
2021-10-26 19:00:23 -0700

    added optional title

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,13 @@ submitBtn.click(submitClick)
 function submitClick() {
 
     let timeInput = dayjs(dateTime.val());
+    let titleInput = $("#titleInput").val();
     if (dayjs(timeInput).isValid()) {
-        let epochTime = "<t:" + dayjs(timeInput).unix() + ":F>"
-        results.text(epochTime);
+        let epochTime = "<t:" + dayjs(timeInput).unix() + ":F>";
+        results.text(epochTime +"\n" + "\n"
+        + titleInput); 
+    }
+    else{
+        alert("Please select a valid date and time.")
     }
 }
