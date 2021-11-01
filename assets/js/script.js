5bcc0e4ed19be0d9f6e35e059db65cdcde5d3736
Cosdaman <Cosdaguy@gmail.com>
2021-10-31 17:43:00 -0700

    added saving of time preference

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 console.log("js script connected")
 
 //dom selection
-dateTime = $("#dateTime");
-submitBtn = $("#submitBtn");
-results = $("#results");
-faraamQuoteDisplay = $("#faraamQuote");
-mythicBtn = $("#mythicBtn");
-mythicCounter = $("#mythicCounter");
+let dateTime = $("#dateTime");
+let submitBtn = $("#submitBtn");
+let results = $("#results");
+let faraamQuoteDisplay = $("#faraamQuote");
+let mythicBtn = $("#mythicBtn");
+let mythicCounter = $("#mythicCounter");
 
+//placeholder vars
+let datePref;
+
+//data variables
 let faraamQuotes = [
     "https://cdn.discordapp.com/attachments/799488351543361547/904505982498013194/ffxiv_10212021_185012_871.png",
     "https://cdn.discordapp.com/attachments/799488351543361547/904505983269740564/ffxiv_10212021_191133_936.png",
@@ -24,6 +28,7 @@ let faraamQuotes = [
     "https://cdn.discordapp.com/attachments/799488351543361547/904507682424250418/ffxiv_10232021_204849_470.png"
 ]
 
+//process variables
 let randomFaraamQuote = faraamQuotes[Math.floor(Math.random() * faraamQuotes.length)];
 
 function submitClick() {
@@ -35,6 +40,7 @@ function submitClick() {
 
     if (titleInput != "") { titleInput = "\n\n" + "**" + titleInput + "**"; }
     if (descInput != "") { descInput = "\n\n" + descInput; }
+
     if (dayjs(timeInput).isValid()) {
         let epochTime = "<t:" + dayjs(timeInput).unix() + ":" + timeFormat + ">";
         results.text(epochTime + titleInput + descInput);
@@ -42,6 +48,9 @@ function submitClick() {
     else {
         alert("Please select a valid date and time.")
     }
+    
+    datePref = timeFormat;
+    localStorage.setItem("datePreference", JSON.stringify(datePref));
 }
 
 //count api
@@ -65,6 +74,14 @@ function mythicClickCount() {
 
 //secret set value to 0 link
 //https://api.countapi.xyz/set/mythicCounter/cosdamanv2?value=0
+
+
+let storedDatePref = JSON.parse(localStorage.getItem("datePreference"));
+
+if (storedDatePref !== null || storedDatePref !== undefined) {
+    datePref = storedDatePref;
+    $("[value='"+datePref+"']").attr('selected',"");
+}
 
 faraamQuoteDisplay.attr("src", randomFaraamQuote);
 mythicInitDisplay();
